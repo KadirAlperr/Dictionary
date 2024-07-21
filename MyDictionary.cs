1022b564f85c8a3bd55d47e1bc09661783c84f88
KadirAlperr <alperkadirunver@gmail.com>
2024-07-21 17:31:16 +0300

    Initial commit

---
+++ MyDictionary.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dictionary
+{
+    internal class MyDictionary <Tkey, Tvalue>
+    {
+
+         Tkey[] _keys;
+         Tvalue[] _values;
+
+        public MyDictionary()
+        {
+            _keys = new Tkey[0];
+            _values = new Tvalue[0];
+        }
+
+        
+        public void Add(Tkey keyitem, Tvalue valueitem)
+        {
+            Tkey[] tempKey;
+            Tvalue[] tempvalue;
+
+            tempKey = _keys;
+            tempvalue = _values;
+
+            _keys = new Tkey[_keys.Length + 1];
+            _values = new Tvalue[_values.Length + 1];
+
+            for(int i = 0; i < tempKey.Length; i++)
+            {
+                _keys[i] = tempKey[i];
+                _values[i] = tempvalue[i];
+            }
+
+            _keys[_keys.Length - 1] = keyitem;
+            _values[_values.Length - 1] = valueitem;
+
+        }
+
+        public int count
+        {
+            get { return _keys.Length; }
+        }
+        
+
+        public void Get()
+        {
+            for(int i = 0; i < _keys.Length; i++)
+            {
+                Console.WriteLine(_keys[i]);
+                Console.WriteLine(_values[i]);
+            }
+        }
+
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿
+using Dictionary;
+/*
+Dictionary<int, string> sehirler = new Dictionary<int, string>();
+
+sehirler.Add(70, "Karaman");
+sehirler.Add(09, "Aydın");
+sehirler.Add(33, "Mersin");
+
+sehirler.Add(06, "Ankara");
+
+sehirler.Remove(09);
+
+foreach(var sehir in sehirler)
+{
+    Console.WriteLine("plaka : " + sehir.Key + " şehir : " + sehir.Value);
+}
+
+bool VarMi = sehirler.ContainsKey(90);
+Console.WriteLine(VarMi);
+
+sehirler[09] = "Yeni Aydın";
+Console.WriteLine(sehirler[09]);
+
+Console.WriteLine("**********************************************************");
+*/
+
+
+
+
+MyDictionary<int, string> myDictionary = new MyDictionary<int, string>();
+myDictionary.Add(33, "Mersin");
+myDictionary.Add(70, "Karaman");
+myDictionary.Add(9, "Aydın");
+myDictionary.Get();
